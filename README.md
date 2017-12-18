5b4835222a7b14bc52a1645dd1b5fd905cf852be
Ian Altgilbers <ian@altgilbers.com>
2017-12-18 13:28:49 -0500

    cleanup/reformatting...  added admin-check for safety

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wpbs
 WordPress Bulk Subscriber Plugin
+
+Simple plugin to allow administator of WordPress network install to bulk-add existing users to sites by providing a list of usernames or email addresses.
+
+User accounts must already exist, and if a user already has a role in the targeted site, no change is made.
--- bs.php
+++ bs.php
@@ -16,22 +16,25 @@ Domain Path:
 wp_enqueue_script("jquery");
 
 add_action('network_admin_menu', 'bs_plugin_setup_menu');
- 
+
 function bs_plugin_setup_menu(){
-        add_menu_page( 
+	add_menu_page(
 		$page_title='Bulk Subscriber Plugin Page',
-                        $menu_title='Bulk Subscriber Plugin',
-                        $capability='manage_network',
-                        $menu_slug='bs-options',
-                        $function='bs_init' );
+		$menu_title='Bulk Subscriber Plugin',
+		$capability='manage_network',
+		$menu_slug='bs-options',
+		$function='bs_init'
+	);
 }
- 
+
 
 add_action('network_admin_edit_bs_process', 'bs_process');
 
 // this function does the business of actually adding users to the site
 function bs_process()
 {
+	if(! is_network_admin())
+		exit;
 	$output="";
 	// would be wise to sanitize input here out of abundance of caution, but form is only accessible to users with super-admin privs..
 	$site=$_POST['site_id'];
@@ -39,11 +42,10 @@ function bs_process()
 	// split user list on whitespace, comma, or semicolon..
 	$users = preg_split( "/[,;\r\n\s]/", $_POST['users'],-1,PREG_SPLIT_NO_EMPTY );
 
-
 	$site_obj=get_site($site);
 	date_default_timezone_set('America/New_York');
 	$output.=date(DATE_RFC2822)."\n\n";
-        $output.="Site selected ID: ".$site."  path: ".$site_obj->path."\n";
+	$output.="Site selected ID: ".$site."  path: ".$site_obj->path."\n";
 	error_log("number of users provided: ".count($users));
 	$output.="Number of users provided: ".count($users)."\n";
 	$output.="Role selected: ".$target_role."\n";
@@ -67,21 +69,23 @@ function bs_process()
 		}
 		else
 		{
-	   	if(is_user_member_of_blog( $u->ID, $site ))
-        	{
-			error_log($u->user_login." already a member of blog");
-   			$output.=$u->user_login." already a member of blog\n";
-		}
-		else
-       			if (add_user_to_blog($site,$u->ID,$target_role))
+			if(is_user_member_of_blog( $u->ID, $site ))
 			{
-				error_log($u->user_login." added to blog with role: ".$target_role);
-                        	$output.=$u->user_login." added to blog with role: ".$target_role."\n";
+				error_log($u->user_login." already a member of blog");
+				$output.=$u->user_login." already a member of blog\n";
 			}
-       			else
-                	{
-	                        error_log($u->user_login.": error adding to blog");
-        	                $output.=$u->user_login." error adding to blog\n";
+			else
+			{
+				if (add_user_to_blog($site,$u->ID,$target_role))
+				{
+					error_log($u->user_login." added to blog with role: ".$target_role);
+					$output.=$u->user_login." added to blog with role: ".$target_role."\n";
+				}
+				else
+				{
+					error_log($u->user_login.": error adding to blog");
+					$output.=$u->user_login." error adding to blog\n";
+				}
 			}
 		}
 	}
@@ -91,71 +95,70 @@ function bs_process()
 	$redirect_url=add_query_arg(array('page' => 'bs-options'),network_admin_url( 'admin.php' ));
 	wp_redirect($redirect_url);
 
-exit;
+	exit;
 }
 
 
 // plugins admin page display
-function bs_init(){
-    ?>
-<h1>Bulk Subscriber</h1>
+function bs_init(){  ?>
+   <h1>Bulk Subscriber</h1>
 
-<form method="post" action="<?php echo network_admin_url( 'edit.php?action=bs_process' ) ?>">
-<p>Choose the site you want to add existing users to from dropdown...  filter the list to help you find the site you want:</p>
-<input id="sites_filter" type="text" placeholder="filter blogs list"/>
-<br/>
-	 <select name="site_id" id="sites_list">
-	  <?php foreach(get_sites(array(number=>100000,orderby=>'registered',order=>'DESC')) as $x)
-	  {
-    		echo "<option value=".$x->blog_id.">".$x->path."</option>";
-  	}  ?>
-	</select>
-<br/>
-<select name="target_role">
-<?php wp_dropdown_roles( 'editor' );?>
-</select>
-<br/>
-<textarea rows="14" cols="50" name="users" placeholder="usernames or emails"></textarea>
+   <form method="post" action="<?php echo network_admin_url( 'edit.php?action=bs_process' ) ?>">
+      <p>Choose the site you want to add existing users to from dropdown...  filter the list to help you find the site you want:</p>
+      <input id="sites_filter" type="text" placeholder="filter blogs list"/>
+      <br/>
+      <select name="site_id" id="sites_list">
+         <?php
+            foreach(get_sites(array(number=>100000,orderby=>'registered',order=>'DESC')) as $x)
+            {
+               echo "<option value=".$x->blog_id.">".$x->path."</option>";
+            }
+         ?>
+      </select>
+      <br/>
+      <select name="target_role">
+         <?php wp_dropdown_roles( 'editor' );?>
+      </select>
+      <br/>
+      <textarea rows="14" cols="50" name="users" placeholder="usernames or emails"></textarea>
+      <?php submit_button();?>
+   </form>
+   <h3>Log of last run...</h3>
+   <pre style="border:1px solid black;"> <?php echo get_site_option('bs_last_log') ?> </pre>
 
-<?php	submit_button();?>
-</form>
-<h3>Log of last run...</h3>
-<pre style="border:1px solid black;"> <?php echo get_site_option('bs_last_log') ?> </pre>
-<script>
-jQuery.fn.filterByText = function(textbox, selectSingleMatch) {
-  return this.each(function() {
-    var select = this;
-    var options = [];
-    jQuery(select).find('option').each(function() {
-      options.push({value: jQuery(this).val(), text: jQuery(this).text()});
-    });
-    jQuery(select).data('options', options);
-    jQuery(textbox).bind('change keyup', function() {
-      var options = jQuery(select).empty().scrollTop(0).data('options');
-      var search = jQuery.trim(jQuery(this).val());
-      var regex = new RegExp(search,'gi');
- 
-      jQuery.each(options, function(i) {
-        var option = options[i];
-        if(option.text.match(regex) !== null) {
-          jQuery(select).append(
-             jQuery('<option>').text(option.text).val(option.value)
-          );
-        }
+   <script>
+      jQuery.fn.filterByText = function(textbox, selectSingleMatch) {
+         return this.each(function() {
+            var select = this;
+            var options = [];
+            jQuery(select).find('option').each(function() {
+               options.push({value: jQuery(this).val(), text: jQuery(this).text()});
+            });
+            jQuery(select).data('options', options);
+            jQuery(textbox).bind('change keyup', function() {
+            var options = jQuery(select).empty().scrollTop(0).data('options');
+            var search = jQuery.trim(jQuery(this).val());
+            var regex = new RegExp(search,'gi');
+
+            jQuery.each(options, function(i) {
+               var option = options[i];
+               if(option.text.match(regex) !== null) {
+                  jQuery(select).append(
+                     jQuery('<option>').text(option.text).val(option.value)
+                  );
+               }
+            });
+            if (selectSingleMatch === true && jQuery(select).children().length === 1) {
+               jQuery(select).children().get(0).selected = true;
+            }
+         });
       });
-      if (selectSingleMatch === true && 
-          jQuery(select).children().length === 1) {
-        jQuery(select).children().get(0).selected = true;
-      }
-    });
-  });
-};
-jQuery(function() {
-  jQuery('#sites_list').filterByText(jQuery('#sites_filter'), true);
-});  
+   };
+   jQuery(function() {
+      jQuery('#sites_list').filterByText(jQuery('#sites_filter'), true);
+   });  
 
-</script>
-</script>
+   </script>
 <?php
 } 
 ?>
