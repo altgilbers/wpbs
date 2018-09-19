f3ac28b8d70c5a05b37c4620b509fad073d21307
Ian Altgilbers <ian@altgilbers.com>
2018-09-19 14:05:23 -0400

    updated to allow for multiple selection of sites

--- bs.php
+++ bs.php
@@ -3,12 +3,21 @@
 Plugin Name:  Bulk Subscriber
 Plugin URI:   https://github.com/altgilbers/wp_bulk_subscriber
 Description:  Bulk Subscribe lists of existing users into a role in an existing site in a WordPress network install
-Version:      1.0
+Version:      1.1
 Author:       Ian Altgilbers
 Author URI:   http://altgilbers.com
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 */
+
+
+/*
+Changes
+
+2018-09-19 - updated to allow for the selection of multiple sites
+
+*/
+
 
 // to make sure jquery is loaded..
 wp_enqueue_script("jquery");
@@ -35,64 +44,75 @@ function bs_process()
 		exit;
 	$output="";
 	// would be wise to sanitize input here out of abundance of caution, but form is only accessible to users with super-admin privs..
-	$site=$_POST['site_id'];
+	// this should be an array, since the actual parameter name is "site_id[]"
+	$sites=$_POST['site_id'];
+
 	$target_role=$_POST['target_role'];
+
 	// split user list on whitespace, comma, or semicolon..
 	$users = preg_split( "/[,;\r\n\s]/", $_POST['users'],-1,PREG_SPLIT_NO_EMPTY );
+	
+        date_default_timezone_set('America/New_York');
+        $output.=date(DATE_RFC2822)."\n\n";
+        $output.="Number of users provided: ".count($users)."\n";
+        $output.="Number of sites provided: ".count($sites)."\n";
+        $output.="Role selected: ".$target_role."\n";
+        $output.="\n\n";
 
-	$site_obj=get_site($site);
-	date_default_timezone_set('America/New_York');
-	$output.=date(DATE_RFC2822)."\n\n";
-	$output.="Site selected ID: ".$site."  path: ".$site_obj->path."\n";
-	error_log("number of users provided: ".count($users));
-	$output.="Number of users provided: ".count($users)."\n";
-	$output.="Role selected: ".$target_role."\n";
-	$output.="\n\n";
-
-	foreach($users as $user)
+	foreach($sites as $site)
 	{
-		if(strpos($user,'@')===FALSE)
-		{
-			$u=get_user_by('login',$user);
-		}
-		else
-		{
-			$u=get_user_by('email',$user);
-		}
 
-		if($u===FALSE)
+		$site_obj=get_site($site);
+		$output.="Site selected ID: ".$site."  path: ".$site_obj->path."\n";
+
+
+		foreach($users as $user)
 		{
-			error_log("user: [".$user."] not found");
-			$output.="user: [".$user."] not found\n";
-		}
-		else
-		{
-			if(is_user_member_of_blog( $u->ID, $site ))
+			if(strpos($user,'@')===FALSE)
 			{
-				error_log($u->user_login." already a member of blog");
-				$output.=$u->user_login." already a member of blog\n";
+				$u=get_user_by('login',$user);
 			}
 			else
 			{
-				if (add_user_to_blog($site,$u->ID,$target_role))
+				$u=get_user_by('email',$user);
+			}
+	
+			if($u===FALSE)
+			{
+				error_log("user: [".$user."] not found");
+				$output.="user: [".$user."] not found\n";
+			}
+			else
+			{
+				if(is_user_member_of_blog( $u->ID, $site ))
 				{
-					error_log($u->user_login." added to blog with role: ".$target_role);
-					$output.=$u->user_login." added to blog with role: ".$target_role."\n";
+					error_log($u->user_login." already a member of blog");
+					$output.=$u->user_login." already a member of blog\n";
 				}
 				else
 				{
-					error_log($u->user_login.": error adding to blog");
-					$output.=$u->user_login." error adding to blog\n";
+					if (add_user_to_blog($site,$u->ID,$target_role))
+					{
+						error_log($u->user_login." added to blog with role: ".$target_role);
+						$output.=$u->user_login." added to blog with role: ".$target_role."\n";
+					}
+					else
+					{
+						error_log($u->user_login.": error adding to blog");
+						$output.=$u->user_login." error adding to blog\n";
+					}
 				}
 			}
 		}
+		$output.="\n";
 	}
 
 	// can't pass any significant amount of info back to the calling page, so i'll store the output in a site option and read it out after.
 	update_site_option('bs_last_log',$output);
+	error_log($output);
+
 	$redirect_url=add_query_arg(array('page' => 'bs-options'),network_admin_url( 'admin.php' ));
 	wp_redirect($redirect_url);
-
 	exit;
 }
 
@@ -105,7 +125,7 @@ function bs_init(){  ?>
       <p>Choose the site you want to add existing users to from dropdown...  filter the list to help you find the site you want:</p>
       <input id="sites_filter" type="text" placeholder="filter blogs list"/>
       <br/>
-      <select name="site_id" id="sites_list">
+      <select name="site_id[]" id="sites_list" multiple>
          <?php
             foreach(get_sites(array(number=>100000,orderby=>'registered',order=>'DESC')) as $x)
             {
